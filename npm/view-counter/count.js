b47c250aca667c7e520d0c02999a85131ff2f7e9
YuXiaoyuYo <YuXiaoyuYo@users.noreply.github.com>
2024-10-15 14:43:33 +0800

    deploy: YuXiaoyuYo/yuxiaoyu-blog@840ac2bfabdd6af234caaee2fa081a9ff655fcaf

--- npm/view-counter/count.js
+++ npm/view-counter/count.js
@@ -1,20 +1,5 @@
 const workerUrl = 'https://yu.nm.cn/api/count';
 
-async function updateViewCount(page, elementId) {
-    const element = document.getElementById(elementId);
-    if (element) {
-        const response = await fetch(workerUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ action: 'increment', page })
-        });
-        const count = await response.text();
-        element.innerText = count;
-    }
-}
-
 async function getTotalViewCount(elementId) {
     const element = document.getElementById(elementId);
     if (element) {
@@ -29,10 +14,6 @@ async function getTotalViewCount(elementId) {
         element.innerText = total;
     }
 }
-
-// 调用函数更新计数并显示最新计数（如果存在）
-const page = window.location.pathname;
-updateViewCount(page, 'view-count');
 
 // 调用函数获取全站总浏览量并显示（如果存在）
 getTotalViewCount('total-view-count');
