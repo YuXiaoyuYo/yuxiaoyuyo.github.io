16bf7cb60556f9dbc514ef26b35c5a3cb6cdbb6b
Yu Xiaoyu <73048194+YuXiaoyuYo@users.noreply.github.com>
2025-08-17 14:28:04 +0800

    更新 favicon 和头像链接为绝对路径

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -11,7 +11,7 @@ export default defineUserConfig({
   bundler: viteBundler(),
 
   head: [
-    ['link', {rel: 'shortcut icon', href: '/images/favicon.webp'}]
+    ['link', {rel: 'shortcut icon', href: 'https://yu.nm.cn/images/favicon.webp'}]
   ],
 
   theme: plumeTheme({
@@ -35,7 +35,7 @@ export default defineUserConfig({
 
     contributors: {
       mode: 'block',
-      info:[{username: 'YuXiaoyuYo',name: '于晓鱼', alias: 'YuXiaoyu', avatar: '/images/favicon.webp',}],
+      info:[{username: 'YuXiaoyuYo',name: '于晓鱼', alias: 'YuXiaoyu', avatar: 'https://yu.nm.cn/images/favicon.webp',}],
     },
 
     replaceAssets: 'https://yu.nm.cn',
